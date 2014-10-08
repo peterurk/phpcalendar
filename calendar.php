2952351a3073bdeb4c59880f69cf5bbe97173783
Peter Post <peter@webdsign.nl>
2014-10-08 10:04:30 +0200

    Made it alot simpler

--- calendar.php
+++ calendar.php
@@ -1,138 +1,44 @@
+
 <?php
 /**
- * Calendar Class
+ * PHP Calendar
  */
-Class calendar
+
+class Calendar
 {
 
-	/**
-	 * Holds all calendar data
-	 * @var array
-	 */
-	private $_data;
+	private $data;
 
 	/**
-	 * Contains all parameters
-	 * @var array
+	 * 
 	 */
-	public $_params;
-
-	/**
-	 * Constructor
-	 */
-	public function __construct() 
+	public function __construct($config = false)
 	{
-		$this->_data = array();
+
 	}
 
-	/**
-	 * Returns the calendar array
-	 * 
-	 * @return array calendar array
-	 */
-	public function getCurrent() 
+	public function create($year = false)
 	{
-		// Fill data array with month numbers
-		for ($m = 1;$m < 13;$m++) {
-			$this->_data[$m] = array();
+		if (false === $year) {
+			$now = new DateTime('now');
+			$year = $now->format('Y');
 		}
 
-		// Fill month array with days
-		foreach ($this->_data as $key => $month) {
-			// Fill array with blanks until we have the correct day of the week
-			for ($blanks = 0;$blanks != date('w', mktime(0, 0, 0, $key, 1, date('Y')));$blanks++) {
-				$this->_data[$key][] = " ";
-			}
-
-			// Add days to month array
-			for ($day = 1;$day != (date("t", mktime(0, 0, 0, $key, 1, date('Y')))+1);$day++) {
-				$this->_data[$key][] = $day;
-			}
-
-			// Fill gap at the end with blanks as well
-			$ceil = ((ceil((count($this->_data[$key])/7))*7)-count($this->_data[$key]));
-			for ($blanks = 0;$blanks != $ceil;$blanks++) {
-				$this->_data[$key][] = " ";
+		$range = range(1, 12);
+		$this->data = array_combine($range, $range);
+		foreach ($this->data as $m) {
+			$date = DateTime::createFromFormat('m::Y', $m.'::'.$year);
+			$range = range(1, $date->format('t'));
+			$this->data[$m] = array_combine($range, $range);
+			foreach ($this->data[$m] as $d) {
+				$date = DateTime::createFromFormat('m-d::Y', $m.'-'.$d.'::'.$year);
+				$this->data[$m][$d] = $date->format('w');
 			}
 		}
-		
-		return $this->_data;
 	}
 
-	/**
-	 * Get a specific month
-	 * 
-	 * @param  string $month Month you want to select
-	 * @param  string $year  Year you want to select the month from
-	 * @return array         The Calendar array
-	 */
-	public function getMonth($month = TRUE, $year = TRUE) 
+	public function render()
 	{
-		if (!is_numeric($month) || $month < 1 || $month > 12) {
-			$month = date('n');
-		}
-
-		if (!is_numeric($year) || $year < 0 || $year > 2050) {
-			$year = date('Y');
-		}
-
-		// Fill month array with days
-
-		// Fill array with blanks until we have the correct day of the week
-		for ($blanks = 0;$blanks != date('w', mktime(0, 0, 0, $month, 1, $year));$blanks++) {
-			$this->_data[] = " ";
-		}
-
-		// Add days to month array
-		for ($day = 1;$day != (date("t", mktime(0, 0, 0, $month, 1, $year))+1);$day++) {
-			$this->_data[] = $day;
-		}
-
-		// Fill gap at the end with blanks as well
-		$ceil = ((ceil((count($this->_data)/7))*7)-count($this->_data));
-		for ($blanks = 0;$blanks != $ceil;$blanks++) {
-			$this->_data[] = " ";	
-		}
-
-		return $this->_data;
-	}
-
-	/**
-	 * Get calendar by a specific year
-	 * 
-	 * @param string $year Year you want to get
-	 * @return array       The Calendar array
-	 */
-	public function getYear($year = TRUE)
-	{
-		if (!is_numeric($year) || $year < 0 || $year > 2050) {
-			$year = date('Y');
-		}
-
-		// Fill data array with month numbers
-		for ($m = 1;$m < 13;$m++) {
-			$this->_data[$m] = array();
-		}
-
-		// Fill month array with days
-		foreach ($this->_data as $key => $month) {
-			// Fill array with blanks until we have the correct day of the week
-			for ($blanks = 0;$blanks != date('w', mktime(0, 0, 0, $key, 1, $year));$blanks++) {
-				$this->_data[$key][] = " ";
-			}
-
-			// Add days to month array
-			for ($day = 1;$day != (date("t", mktime(0, 0, 0, $key, 1, $year))+1);$day++) {
-				$this->_data[$key][] = $day;
-			}
-
-			// Fill gap at the end with blanks as well
-			$ceil = ((ceil((count($this->_data[$key])/7))*7)-count($this->_data[$key]));
-			for ($blanks = 0;$blanks != $ceil;$blanks++) {
-				$this->_data[$key][] = " ";	
-			}
-		}
-
-		return $this->_data;
+		return $this->data;
 	}
 }
